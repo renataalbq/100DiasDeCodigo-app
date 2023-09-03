75a6150e83eabca7d023b5cd75326eaff4938927
João Victor <joaovictorvisona@gmail.com>
2023-09-03 14:11:44 -0300

    fixed wrong redirect uri on twitter login

--- src/screens/sign-in/sign-in.tsx
+++ src/screens/sign-in/sign-in.tsx
@@ -5,45 +5,47 @@ import { View } from 'react-native';
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
 
+WebBrowser.maybeCompleteAuthSession();
+
+const CLIENT_ID = process.env.EXPO_PUBLIC_CLIENT_ID;
+if (!CLIENT_ID) {
+  throw new Error('EXPO_PUBLIC_CLIENT_ID is required');
+}
+
+const CLIENT_SECRET = process.env.EXPO_PUBLIC_CLIENT_SECRET;
+if (!CLIENT_SECRET) {
+  throw new Error('EXPO_PUBLIC_CLIENT_SECRET is required');
+}
+
+const twitterUrl = {
+  authorizationEndpoint: 'https://twitter.com/i/oauth2/authorize',
+  tokenEndpoint: 'https://twitter.com/i/oauth2/token',
+  revocationEndpoint: 'https://twitter.com/i/oauth2/revoke',
+};
+
 export const SignIn = () => {
-  const navigation = useNavigation();
-
-  const CLIENT_ID = process.env.CLIENT_ID;
-  const CLIENT_SECRET = process.env.CLIENT_SECRET;
-
-  WebBrowser.maybeCompleteAuthSession();
-  const twitterUrl = {
-    authorizationEndpoint: "https://twitter.com/i/oauth2/authorize",
-    tokenEndpoint: "https://twitter.com/i/oauth2/token",
-    revocationEndpoint: "https://twitter.com/i/oauth2/revoke",
-  };
-
   const [request, response, promptAsync] = AuthSession.useAuthRequest(
     {
       clientId: CLIENT_ID,
       clientSecret: CLIENT_SECRET,
-      redirectUri: AuthSession.makeRedirectUri({
-        scheme: 'cemdiasdecodigo',
-      }),
+      redirectUri: 'cemdiasdecodigo://',
       usePKCE: true,
-      scopes: [
-        "tweet.read",
-        "offline.access",
-        "like.read",
-        "users.read",
-
-      ],
+      scopes: ['tweet.read', 'offline.access', 'like.read', 'users.read'],
     },
-    twitterUrl
+    twitterUrl,
   );
 
   const handleSignIn = () => {
-    promptAsync
+    promptAsync();
   };
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Button.Primary text='Entrar com o twitter'  onTap={handleSignIn} />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button.Primary
+        loading={!request}
+        text='Entrar com o twitter'
+        onTap={handleSignIn}
+      />
     </View>
   );
 };
